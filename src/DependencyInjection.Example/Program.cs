ad97e7cabfc146c24230bab8cf840d8a72f5054a
Eduardo Salvador <eduardo.salvador@col.vueling.com>
2021-09-03 15:00:33 +0200

    Creates simple case to show IoC

---
+++ src/DependencyInjection.Example/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DependencyInjection.Example
+{
+	public interface IPrinter
+	{
+		void Print(string caller, string message);
+	}
+
+	public interface IOperationService
+	{
+		void DoSomeRealWork();
+	}
+
+	public class Printer : IPrinter
+	{
+		public void Print(string caller, string message)
+		{
+			Console.WriteLine($"[ {caller}...{message} ]");
+		}
+	}
+
+	public class OperationService : IOperationService
+	{
+		private IPrinter _printer;
+
+		public OperationService(IPrinter printer)
+		{
+			_printer = printer;
+		}
+
+		public void DoSomeRealWork()
+		{
+			_printer.Print("OperationService", "Print DoSomeRealWork");
+		}
+	}
+
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			//setup our DI
+			var serviceProvider = new ServiceCollection()
+				.AddSingleton<IPrinter, Printer>()
+				.AddSingleton<IOperationService, OperationService>()
+				.BuildServiceProvider();
+
+			Console.WriteLine("Starting application");
+			IOperationService service = serviceProvider.GetService<IOperationService>();
+			service.DoSomeRealWork();
+			Console.WriteLine("All done!");
+		}
+	}
+}
